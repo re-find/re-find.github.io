4e789a01b97cd2196d6ea454c4ae1c8f240729df
Yury Matuhin <ymatuhin@yandex.ru>
2015-03-22 13:44:09 +0300

    upd| add instantclick

--- js/code.js
+++ js/code.js
@@ -1,24 +1,3 @@
 var el = document.getElementsByTagName('body')[0];
 el.innerHTML = el.innerHTML.replace(/«/g, '<span class="slaquo"> </span> <span class="hlaquo"> «</span>')
 el.innerHTML = el.innerHTML.replace(/\(/g, '<span class="sbrace"> </span> <span class="hbrace"> (</span>')
-
-// Manifest reloader
-window.addEventListener('load', function(e) {
-	window.applicationCache.addEventListener('updateready', function(e) {
-		if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-			window.location.reload();
-		}
-	}, false);
-}, false);
-
-function loadCSS (path) {
-	var head  = document.getElementsByTagName('head')[0];
-	var link  = document.createElement('link');
-	link.rel  = 'stylesheet';
-	link.type = 'text/css';
-	link.href = path;
-	link.media = 'all';
-	head.appendChild(link);
-}
-
-loadCSS('/css/main.css')
